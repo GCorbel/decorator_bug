617fbbf0cdfc4b39c995bbb629e84e50e4ab3e98
GCorbel <guirec.corbel@gmail.com>
2017-05-14 06:42:48 -0400

    Reproduce the bug

---
+++ tests/unit/routes/blabla-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:blabla', 'Unit | Route | blabla', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
